823b57987400093c963f00ab1c3cc85459a4bbda
Guilherme Bialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-06-30 17:34:16 -0300

    Add files via upload

---
+++ Exercicio03Lista08.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExerciciosLista08;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Guilherme Bialas
+ */
+public class Exercicio03Lista08 {
+
+    private String marca;
+    private double comprimento, largura, altura, preco;
+    private boolean isufilm;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setComprimento(Double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public Double getComprimento() {
+        return comprimento;
+    }
+
+    public void setLargura(Double largura) {
+        this.largura = largura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setIsufilm(Boolean isufilm) {
+        this.isufilm = isufilm;
+    }
+
+    public Boolean getIsufilm() {
+        return isufilm;
+    }
+
+    
+}
